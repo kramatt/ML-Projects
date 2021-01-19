0d89ea9582f58e686f0337e253822f95abe048b5
Matt <matthew.kramer@uconn.edu>
2021-01-18 19:03:20 -0500

    import all the regressors

--- FSAE Time Predictions/fsae-time-predictions.py
+++ FSAE Time Predictions/fsae-time-predictions.py
@@ -4,6 +4,18 @@ import matplotlib.pyplot as plt
 from sklearn.model_selection import KFold
 from sklearn.model_selection import cross_val_score
 from sklearn.linear_model import LogisticRegression
+from slkearn.linear_model import SGDRegressor
+from sklearn.linear_model import ElasticNet
+from sklearn.linear_model import Lasso
+from sklearn.linear_model import Ridge
+from sklearn.svm import SVR #try kernel=linear and kernel=rbf
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.ensemble import ExtraTreesRegressor
+from sklearn.ensemble import AdaBoostRegressor
+from sklearn.ensemble import GradientBoostingRegressor
+from sklearn.ensemble import HistGradientBoostingRegressor
+from sklearn.ensemble import VotingRegressor
+from skelarn.ensemble import StackingRegressor
 
 file = 'Reduced Data/fsae-accel-skidpad-times.csv'
 dataframe = pd.read_csv(file)

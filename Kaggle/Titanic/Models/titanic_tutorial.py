3c8cf4d97fd9a4f25dac1eafc94d1a23edbb5973
Matt <matthew.kramer@uconn.edu>
2021-01-26 20:45:37 -0500

    add kaggle comp

---
+++ Kaggle/Titanic/Models/titanic_tutorial.py
@@ -0,0 +1,22 @@
+# Predictive model for Kaggle's Titanic Competition (Tutorial Version)
+# Author: Matthew Kramer
+
+import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
+
+train_data = pd.read_csv('../Datasets/train.csv', header=0)
+test_data = pd.read_csv('../Datasets/test.csv', header=0)
+
+y = train_data['Survived']
+
+features = ['Pclass', 'Sex', 'SibSp', 'Parch']
+X = pd.get_dummies(train_data[features])
+X_test = pd.get_dummies(test_data[features])
+
+model = RandomForestClassifier(n_estimators=100, max_depth=5, random_state=1)
+model.fit(X, y)
+predictions = model.predict(X_test)
+
+output = pd.DataFrame({'PassengerId': test_data.PassengerId, 'Survived': predictions})
+output.to_csv('../Submissions/my_submission.csv', index=False)
+print('Your submission was successfully saved!')

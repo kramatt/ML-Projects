9ca4f8f561d76307e211c426ad05e3781d43cfc9
Matt <matthew.kramer@uconn.edu>
2021-01-28 17:14:29 -0500

    add embarked to features

--- Kaggle/Titanic/Models/titanic_randomforest.py
+++ Kaggle/Titanic/Models/titanic_randomforest.py
@@ -2,14 +2,20 @@
 # Author: Matthew Kramer
 
 import pandas as pd
+import matplotlib.pyplot as plt
 from sklearn.ensemble import RandomForestClassifier
+
+pd.options.display.width = 0
 
 train_data = pd.read_csv('../Datasets/train.csv', header=0)
 test_data = pd.read_csv('../Datasets/test.csv', header=0)
 
+pd.plotting.scatter_matrix(train_data)
+plt.show()
+
 y = train_data['Survived']
 
-features = ['Pclass', 'Sex', 'SibSp', 'Parch']
+features = ['Pclass', 'Sex', 'SibSp', 'Parch', 'Embarked']
 X = pd.get_dummies(train_data[features])
 X_test = pd.get_dummies(test_data[features])
 
@@ -18,5 +24,5 @@ model.fit(X, y)
 predictions = model.predict(X_test)
 
 output = pd.DataFrame({'PassengerId': test_data.PassengerId, 'Survived': predictions})
-output.to_csv('../Submissions/my_submission.csv', index=False)
+output.to_csv('../Submissions/randomforest_addembarked.csv', index=False)
 print('Your submission was successfully saved!')

b059c98d99ad0264f6d3dee71990883139c3badc
Matt <matthew.kramer@uconn.edu>
2021-02-16 07:08:49 -0500

    tuning n_estimators

--- Kaggle/Playground 21.2/Models/Kaggle Playground 21.2.py
+++ Kaggle/Playground 21.2/Models/Kaggle Playground 21.2.py
@@ -3,7 +3,9 @@
 # Accuracy scored with RMSE
 
 import pandas as pd
+import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
+from sklearn.model_selection import cross_val_score
 from sklearn.metrics import mean_squared_error
 from xgboost import XGBRegressor
 
@@ -33,18 +35,39 @@ X_train, X_valid = X_train.align(X_valid, join='left', axis=1)
 X_train, X_test = X_train.align(X_test, join='left', axis=1)
 
 
+# Tuning hyperparameters
+def get_score(n_estimators, X, y):
+    # Inputs:
+    #   n_estimators to use for XGBoost model
+    #   X array to use as model input features
+    #   y array to use as model input labels
+    # Returns:
+    #   average RMSE over 3 cv folds of XGBoost model
+    
+    model = XGBRegressor(n_estimators=n_estimators, random_state=0)
+    scores = -1 * cross_val_score(model, X, y, cv=3, n_jobs=-1, scoring='neg_mean_squared_error')
+    print(n_estimators, scores.mean())
+    return scores.mean()
+
+cv_results = {}
+for i in range(10, 60, 10):
+    cv_results[i] = get_score(i, X_train, y_train)
+
+plt.plot(list(cv_results.keys()), list(cv_results.values()))
+plt.show()
+
 # Create and fit model
-model = XGBRegressor()
-model.fit(X_train, y_train)
+#model = XGBRegressor()
+#model.fit(X_train, y_train)
 
 
 # Make validiation predictions and calculate accuracy
-valid_preds = model.predict(X_valid)
-print('RMSE:', mean_squared_error(valid_preds, y_valid))
+#valid_preds = model.predict(X_valid)
+#print('RMSE:', mean_squared_error(valid_preds, y_valid))
 
 
 # Make test predictions and create .csv submission
-test_preds = model.predict(X_test)
-output = pd.DataFrame({'id': test_data.id, 'target': test_preds})
-output.to_csv('../Submissions/submission_playground_21_2.csv', index=False)
-print('Submission saved!')
+#test_preds = model.predict(X_test)
+#output = pd.DataFrame({'id': test_data.id, 'target': test_preds})
+#output.to_csv('../Submissions/submission_playground_21_2.csv', index=False)
+#print('Submission saved!')

fcccdc6661b6b341a533c7dff5cc23ca1ed51390
Matt <matthew.kramer@uconn.edu>
2021-03-02 08:29:33 -0500

    submission with eta=0.2

--- Kaggle/Playground 21.3/Models/tps_mar_2021.py
+++ Kaggle/Playground 21.3/Models/tps_mar_2021.py
@@ -45,35 +45,35 @@ def get_score(eta, X, y):
     # Returns:
     #   average roc_auc over 5 cv folds of XGBoost model
     
-    model = XGBRegressor(eta, random_state=0)
+    model = XGBRegressor(eta=eta, random_state=0)
     scores = cross_val_score(model, X, y, cv=5, n_jobs=-1, scoring='roc_auc')
     print(eta, scores.mean())
     return scores.mean()
 
 # Tuning parameters - define bounds
-cv_results = {}
-for i in range(0.1, 1.1, 0.1):
-    cv_results[i] = get_score(i, X_train, y_train)
+#cv_results = {}
+#for i in range(10, 35, 5):
+#    cv_results[i/100] = get_score(i/100, X_train, y_train)
 
 # Tuning parameters - plot results
-plt.plot(list(cv_results.keys()), list(cv_results.values()))
-plt.xlabel('eta')
-plt.ylabel('roc_auc')
-plt.title('roc_auc for XGBoost with varying eta')
-plt.show()
+#plt.plot(list(cv_results.keys()), list(cv_results.values()))
+#plt.xlabel('eta')
+#plt.ylabel('roc_auc')
+#plt.title('roc_auc for XGBoost with varying eta')
+#plt.show()
 
 # Create and fit model
-#model = XGBRegressor(n_estimators=75, random_state=0)
-#model.fit(X_train, y_train)
+model = XGBRegressor(eta=0.2, random_state=0)
+model.fit(X_train, y_train)
 
 
 # Make validiation predictions and calculate accuracy
-#valid_preds = model.predict(X_valid)
-#print('roc_auc:', roc_auc_score(y_valid, valid_preds))
+valid_preds = model.predict(X_valid)
+print('roc_auc:', roc_auc_score(y_valid, valid_preds))
 
 
 # Make test predictions and create .csv submission
-#test_preds = model.predict(X_test)
-#output = pd.DataFrame({'id': test_data.id, 'target': test_preds})
-#output.to_csv('../Submissions/submission_playground_21_3.csv', index=False)
-#print('Submission saved!')
+test_preds = model.predict(X_test)
+output = pd.DataFrame({'id': test_data.id, 'target': test_preds})
+output.to_csv('../Submissions/submission_playground_21_3.csv', index=False)
+print('Submission saved!')
